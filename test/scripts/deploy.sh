a183316b9e461f59ede849e3d06e948f359aa4a6
root <root@mks-cluster-master.localdomain>
2025-12-03 02:05:09 +0000

    refactor path

--- test/scripts/deploy.sh
+++ test/scripts/deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-kubectl apply -f test/manifests/nginx-deployment.yaml
+cd "$(dirname "$0")"
+
+echo "Current Directory: $(pwd)"
+echo "Files in this directory:"
+ls -al
+
+# 스크립트가 test/scripts/에 있고, yaml이 test/manifests/에 있다면 '../'로 상위로 가야 함
+kubectl apply -f ../manifests/nginx-deployment.yaml
