ebafb51b0dd0eeae35f1e4acd07f9b7df6393b20
root <root@mks-cluster-master.localdomain>
2025-12-03 01:45:49 +0000

    change path

--- test/scripts/deploy.sh
+++ test/scripts/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo $(pwd)
-echo $(ls -al)
-
-kubectl apply -f https://raw.githubusercontent.com/hjkim/my-repo/main/test/manifests/nginx-deployment.yaml
+kubectl apply -f test/manifests/nginx-deploymen.yaml
